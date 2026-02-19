f39803bed85d73af96a979ec318206ce017bb978
Thales Santana <thales.c.santana@outlook.com>
2026-02-19 11:59:39 -0300

    fix: ensure basePath is set correctly in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/institutional',
     images: {
         unoptimized: true,
         remotePatterns: [
